5a7f21914e19a55dbacf5cc9f105f54c86edba99
wafflydevin <devin.cheriant@gmail.com>
2024-12-07 19:40:43 -0500

    adding config read and write functions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wafflydevin/blog_aggregator
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/wafflydevin/blog_aggregator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("waffles")
+	if err != nil {
+		log.Fatalf("error setting user: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config again: %+v\n", cfg)
+}
